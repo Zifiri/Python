9fe49169282f2ccdbf0a35b4e2498b48d9d1f441
Müjdat Cengiz <mujdat.cengiz@akmyazilim.com>
2018-11-02 17:27:04 +0300

    Data types of Python

---
+++ data-types.py
@@ -0,0 +1,31 @@
+#Strings
+strn = "Ammar"
+
+#Integers
+nums = 20
+
+#Float
+fnum = 20.11
+
+#Boolean
+bl = bool(True)
+
+#List
+lists = [1,2,3,4,5,"Hi",6,7,8,9,"By"]
+
+#Tuple 
+tuples = (1,2,3,4,5,"Hi",6,7,8,9,"By")
+
+#Dict
+dicts = {"Banana":5,"WaterMelon":2}
+
+
+
+#All type of data types
+print(type(strn))
+print(type(nums))
+print(type(fnum))
+print(type(bl))
+print(type(lists))
+print(type(tuples))
+print(type(dicts))
