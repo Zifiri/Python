a2b3893819fd19059f695afcef55396a36abe2e4
Müjdat Cengiz <mujdat.cengiz@akmyazilim.com>
2018-11-07 17:34:20 +0300

    Add basic calculator

---
+++ basic-calculator.py
@@ -0,0 +1,35 @@
+into = """
+Choose an action:
+1 - Addition
+2 - Subtraction
+3 - Multiplication
+4 - Division
+"""
+
+print(into)
+
+choosed = int(input("Enter an action number: "))
+
+if choosed == 1:
+   ch1  = int(input("Enter the first number: "))
+   ch2  = int(input("Enter the second number: "))
+   print("Result:{} + {} = {} ".format(ch1,ch2,ch1+ch2))  
+elif choosed == 2:
+   ch1  = int(input("Enter the first number: "))
+   ch2  = int(input("Enter the second number: "))
+   print("Result:{} - {} = {} ".format(ch1,ch2,ch1-ch2))
+elif choosed == 3:
+   ch1  = int(input("Enter the first number: "))
+   ch2  = int(input("Enter the second number: "))
+   print("Result:{} * {} = {} ".format(ch1,ch2,ch1*ch2))   
+elif choosed == 4:
+   ch1  = int(input("Enter the first number: "))
+   ch2  = int(input("Enter the second number: "))
+   print("Result:{} / {} = {} ".format(ch1,ch2,ch1/ch2))
+else:
+    print("=="*20)
+    print("Wrong choice")
+    print("=="*20)
+    print(into)
+
+
