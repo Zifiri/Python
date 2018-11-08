9f042b3ea2b95740a46ca4aa3363dd8a197c6dca
Müjdat Cengiz <mujdat.cengiz@akmyazilim.com>
2018-11-08 16:29:03 +0300

    add for loop

---
+++ for-loop.py
@@ -0,0 +1,20 @@
+# For Basic
+
+for letter in "Ammar":
+    print(": =  > ", letter)
+
+
+
+
+
+# Loop for loop
+
+nums = [
+    [1, 2, 3, 4, 5], 
+    [5.1, 2.2, 61, 2.56], 
+    ["Ammar", "John", "Nicol"]
+]
+
+for row in nums:
+    for col in row:
+        print(col)
